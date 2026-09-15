b5eb5570c08d2ae32297f31280e83984aaf116e1
karlwarren1 <kgaas@umindanao.edu.ph>
2026-09-15 20:33:20 +0800

    remove price

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -65,13 +65,13 @@
             src="{{ asset('img/' . $plate['img']) }}"
           >
           <div class="menu-item-body reveal">
-            <div class="row">
+            {{-- <div class="row">
               <span class="name">{{ $plate['name'] }}</span>
               <span
                 class="price ph"
                 data-ph="price not in handoff"
               >$—</span>
-            </div>
+            </div> --}}
             <p class="ingredients">{{ $plate['spec'] }}</p>
           </div>
         </div>
