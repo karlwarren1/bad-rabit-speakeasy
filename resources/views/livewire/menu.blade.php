b9e6951d39075f8b655b9894850f91ff8bcf9784
karlwarren1 <kgaas@umindanao.edu.ph>
2026-09-20 10:31:39 +0800

    update

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -2,7 +2,7 @@
   <nav
     aria-label="Menu sections"
     class="menu-nav"
-    style=" margin-top: 2rem"
+    style=" margin-top: 4rem"
   >
     <a href="#libations">Libations</a>
     <a href="#provisions">Provisions</a>
